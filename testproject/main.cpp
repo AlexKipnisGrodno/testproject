679809b92085dc6641ee157e77dd82b86e294335
Kipnis <kipnis.work@gmail.com>
2023-04-15 11:17:56 +0300

    Initial Commit

---
+++ testproject/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  testproject
+//
+//  Created by Алексей Кипнис on 15.04.23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
